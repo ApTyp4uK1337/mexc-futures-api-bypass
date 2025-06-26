ace2a40d4ae46dc1190675e3f4fe054b9c2d6395
ApTyp4uK1337 <aptyp4uk1337@gmail.com>
2025-06-27 00:15:35 +0300

    Live Demo

---
+++ mexc-demo/order.php
@@ -0,0 +1,44 @@
+<?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$raw = file_get_contents('php://input');
+$data = json_decode($raw, true);
+
+if (json_last_error() !== JSON_ERROR_NONE) {
+  echo json_encode([
+    'status' => 'error',
+    'message' => 'Invalid JSON received.'
+  ]);
+  exit;
+}
+
+$required = ['type', 'symbol', 'side', 'quantity', 'leverage', 'token'];
+
+foreach ($required as $key) {
+  if (empty($data[$key])) {
+    echo json_encode([
+      'status' => 'error',
+      'message' => "Missing field: $key"
+    ]);
+
+    exit;
+  }
+}
+
+include("{$_SERVER['DOCUMENT_ROOT']}/MexcClient.php");
+
+$client = new MexcClient($data['token'], true);
+
+$response = $client->createOrder([
+  'symbol' => $data['symbol'],
+  'side' => $data['side'],
+  'type' => 5,
+  'openType' => $data['type'],
+  'side' => $data['side'],
+  'vol' => $data['quantity'],
+  'leverage' => $data['leverage']
+]);
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
